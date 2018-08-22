b42e2c060d87fb4e9c1d7c7bcc38005c3568aee0
OlarAlca <oscar@vauxoo.com>
2018-08-22 16:29:36 -0500

    [ADD] academy: build backend theme

--- academy/static/src/js/academy_tour.js
+++ academy/static/src/js/academy_tour.js
@@ -2,10 +2,11 @@ odoo.define('academy.teachers_tour', function(require){
     'use strict';
 
     var tour = require('web_tour.tour'),
+        core = require('web.core'),
         base = require('web_editor.base');
 
     tour.register('tour_show_biographies',{
-        test: true,
+        // test: true,
         url: '/academy/academy',
         wait_for: base.ready(),
     },
@@ -13,10 +14,12 @@ odoo.define('academy.teachers_tour', function(require){
         {
             content: 'Click on first teacher',
             trigger: 'p[data-teacher-id="1"] a',
+            position: "bottom",
         },{
             content: 'Click on first teacher',
             trigger: 'p[data-teacher-id="2"] a',
             extra_trigger: '.s_comparisons',
+            position: "bottom",
         },
     ]);
 });
