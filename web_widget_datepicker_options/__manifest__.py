3d8e2e46e79a5167d3783f6adba336f4d175e0f2
OlarAlca <oscar@vauxoo.com>
2018-08-23 16:48:28 -0500

    [ADD] showcase of inheritance of backend widgets

---
+++ web_widget_datepicker_options/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Web Widget DatePicker Options',
+    'version': '11.0.0.0.1',
+    'author': 'Vauxoo',
+    'depends': [
+        'web',
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+    'auto_install': False,
+    'installable': True,
+    'web_preload': True,
+}
